19c8e629e19cef1c20605ad09ee959d22b0c4b10
Philippe Ferrara <philippe.ferrara@gmx.ch>
2017-06-13 14:38:54 +0200

    Section 3 commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test recipe', 'this is a test',
+    'https://upload.wikimedia.org/wikipedia/commons/6/6b/Mille-feuille_20100916.jpg'
+  )
+  ];
   constructor() { }
 
   ngOnInit() {
