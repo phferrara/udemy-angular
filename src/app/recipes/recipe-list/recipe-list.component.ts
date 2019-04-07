086ab915116f89d6470eaa21b61e53d1339cd855
Philippe Ferrara <philippe.ferrara@gmx.ch>
2019-04-07 02:20:10 +0200

    Routing added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,20 +9,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('A Test recipe', 'this is a test',
-    'https://upload.wikimedia.org/wikipedia/commons/6/6b/Mille-feuille_20100916.jpg'
-  )
-  ];
-  constructor() { }
+  recipes: Recipe[] = [];
+  
+  constructor(private recipeService: RecipeService, 
+      private router: Router,
+      private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,33 @@
+import { Recipe } from "./recipe.model";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+    
+    private recipes: Recipe[] = [
+        new Recipe('A Test recipe', 'this is a test',
+        'https://upload.wikimedia.org/wikipedia/commons/6/6b/Mille-feuille_20100916.jpg',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+        ]
+      )
+    ];
+
+    constructor(private slService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+}
