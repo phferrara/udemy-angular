cd2dbb5f028c3157bf20393b7e888e8c30b76fa0
Philippe Ferrara <philippe.ferrara@gmx.ch>
2019-04-15 01:13:42 +0200

    Http

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Response } from '@angular/http';
+
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+
+import 'rxjs/Rx';
+
+@Injectable()
+export class DataStorageService {
+    constructor(private http: Http, private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        return this.http.put(
+            'https://ng-recipe-book-9885f.firebaseio.com/recipes.json', 
+            this.recipeService.getRecipes());
+    }
+
+    getRecipes() {
+        this.http.get('https://ng-recipe-book-9885f.firebaseio.com/recipes.json')
+          .map(
+            (response: Response) => {
+              const recipes: Recipe[] = response.json();
+              for (let recipe of recipes) {
+                if (!recipe['ingredients']) {
+                  recipe['ingredients'] = [];
+                }
+              }
+              return recipes;
+            }
+          )
+          .subscribe(
+            (recipes: Recipe[]) => {
+              this.recipeService.setRecipes(recipes);
+            }
+          );
+      }
+}
