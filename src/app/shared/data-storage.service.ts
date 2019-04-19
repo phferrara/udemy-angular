b6dcd17c86dd3b32a0791fc60b892a145c556b63
Philippe Ferrara <philippe.ferrara@gmx.ch>
2019-04-19 21:28:05 +0200

    Migration to Angular 7.2.13 + Authentication

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -6,19 +6,24 @@ import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 
 import 'rxjs/Rx';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) {}
+    constructor(private http: Http, 
+                private recipeService: RecipeService,
+                private authService: AuthService) {}
 
     storeRecipes() {
+        const token = this.authService.getToken();
         return this.http.put(
-            'https://ng-recipe-book-9885f.firebaseio.com/recipes.json', 
+            'https://ng-recipe-book-9885f.firebaseio.com/recipes.json?auth=' + token, 
             this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-9885f.firebaseio.com/recipes.json')
+        const token = this.authService.getToken();
+        this.http.get('https://ng-recipe-book-9885f.firebaseio.com/recipes.json?auth=' + token)
           .map(
             (response: Response) => {
               const recipes: Recipe[] = response.json();
